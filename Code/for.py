b30bd9ab40c485debb272833b764aaa2f4071853
wenbobao <744865006@qq.com>
2017-12-25 17:10:02 +0800

    循环

---
+++ Code/for.py
@@ -0,0 +1,11 @@
+# for-in 循环
+names = ['Michael', 'Bob', 'Tracy']
+for name in names:
+    print(name)
+
+# 0到100和
+sum = 0
+for x in range(101):
+    sum = sum + x
+print(sum)
+
---
+++ Code/while.py
@@ -0,0 +1,24 @@
+# while 循环
+sum = 0
+n = 99
+while n > 0:
+    sum = sum + n
+    n = n - 2
+print(sum)
+
+# break提前退出循环
+n = 1
+while n <= 100:
+    if n > 10: # 当n = 11时，条件满足，执行break语句
+        break # break语句会结束当前循环
+    print(n)
+    n = n + 1
+print('END')
+
+# continue`跳过当前循环
+n = 0
+while n < 10:
+    n = n + 1
+    if n % 2 == 0: # 如果n是偶数，执行continue语句
+        continue # continue语句会直接继续下一轮循环，后续的print()语句不会执行
+    print(n)
