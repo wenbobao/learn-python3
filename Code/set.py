0efe5ae6b8d196e79d451f73d3b89fe3a7f7c1d4
wenbobao <744865006@qq.com>
2017-12-25 17:35:21 +0800

    set

---
+++ Code/set.py
@@ -0,0 +1,14 @@
+# 创建一个set
+s = set([1,2,3])
+print(s)
+# 添加元素
+s.add(4)
+print(s)
+# 移除元素
+s.remove(4)
+print(s)
+
+s = set((1,2,3))
+print(s)
+s.add(4)
+print(s)
---
+++ Post/set.md
@@ -0,0 +1,14 @@
+## set
+
+`set`的key不能重复。
+```
+# 创建一个set
+s = set([1,2,3])
+print(s)
+# 添加元素
+s.add(4)
+print(s)
+# 移除元素
+s.remove(4)
+print(s)
+```
