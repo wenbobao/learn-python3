687525d61c940f8394ed1a1527703caeeac348a6
wenbobao <744865006@qq.com>
2017-12-25 16:04:24 +0800

    条件判断-if-else

---
+++ Code/if_else.py
@@ -0,0 +1,17 @@
+# if-else
+
+age = int(input('输入你的年龄:'))
+if age > 18:
+    print('成年')
+else:
+    print('未成年')
+
+# if-elif-else
+
+age = int(input('输入你的年龄:'))
+if age >= 18:
+    print('成年')
+elif age >= 6:
+    print('少年')
+else:
+    print('儿童')
