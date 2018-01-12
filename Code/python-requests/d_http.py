ef9949bca6f0ac1346104ff51973d444df1eeb22
bsnpbqd <wenbo-wb.bao@aia.com>
2018-01-12 15:33:15 +0800

    Add http

---
+++ Code/python-requests/d_http.py
@@ -0,0 +1,17 @@
+import requests
+import mysql.connector
+ 
+conn = mysql.connector.connect(user='root', password='', database='test')
+cursor = conn.cursor()
+# cursor.execute('create table goods (id varchar(20) primary key, title varchar(200), price varchar(200))')
+
+params = {'type': 'total', 'appkey': '21a9904b36', 'v' : '2', 'page' : '1'} 
+r = requests.get('http://api.dataoke.com/index.php?r=Port/index', params=params)
+items = r.json()['result'];
+print(items)
+
+for item in items:
+  cursor.execute('insert into goods (id, title, price) values (%s, %s, %s)', [ item['ID'], item['Title'], item['Price']])
+  conn.commit()
+
+cursor.close()
