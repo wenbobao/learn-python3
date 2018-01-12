4b735a9ee8996cbd07da40920487519faf4c1e45
bsnpbqd <wenbo-wb.bao@aia.com>
2018-01-12 14:19:40 +0800

    add python_mysql

---
+++ Code/python-mysql/d_mysql.py
@@ -0,0 +1,8 @@
+import mysql.connector
+
+conn = mysql.connector.connect(user='root', password='', database='test')
+cursor = conn.cursor()
+cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+cursor.execute('insert into user (id, name) values (%s, %s)', ['1', 'Michael'])
+conn.commit()
+cursor.close()
